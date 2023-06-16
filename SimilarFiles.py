2506a6bf8a46e66a3cf4b168c060735de74945d7
goncas200 <gmbjoao@gmail.com>
2023-06-16 10:14:23 +0100

    Added option to delete really similar files

--- SimilarFiles.py
+++ SimilarFiles.py
@@ -3,6 +3,7 @@ import os
 def similarFiles(folder: str):
     files = []
     files_iterated = []
+    percentage = 0
     os.chdir(folder)
     for file in os.listdir(folder):
         if os.path.isfile(file):
@@ -21,30 +22,52 @@ def similarFiles(folder: str):
             with open(file2, 'r') as f2:
                 f2 = f2.read()
             if f1 == f2:
-                print(f"{file}/{file2}: 100%")
+                percentage = 100
+                print(f"{file}/{file2}: {percentage}% similar")
+                print(f"Files {file} and {file2} are really similar")
+                answer = input("Do you want to delete one of them? [Y/n]: ")
+                if answer.lower() == 'y' or answer.lower() == 'yes' or answer == '':
+                    if len(f1) > len(f2):
+                        os.remove(file)
+                        print(f"{file} removed")
+                        files.remove(file)
+                    else:
+                        os.remove(file2)
+                        print(f"{file2} removed")
+                        files.remove(file2)
                 continue
             if len(f1) > len(f2):
                 for char in f1:
                     if char in f2:
                         similar_chars += 1
                 if similar_chars != 0:
-                    print(f"{file}/{file2}: {round(similar_chars / len(f1) * 100, 2)}%")
+                    percentage = round(similar_chars / len(f1) * 100, 2)
+                    print(f"{file}/{file2}: {percentage}% similar")
                 else:
-                    print(f"{file}/{file2}: 0%")
+                    percentage = 0
+                    print(f"{file}/{file2}: {percentage}%")
             else:
                 for char in f2:
                     if char in f1:
                         similar_chars += 1
                 if similar_chars != 0:
-                    print(f"{file}/{file2}: {round(similar_chars / len(f2) * 100, 2)}%")
+                    percentage = round(similar_chars / len(f2) * 100, 2)
+                    print(f"{file}/{file2}: {percentage}% similar")
                 else:
-                    print(f"{file}/{file2}: 0%")
-
+                    percentage = 0
+                    print(f"{file}/{file2}: {percentage}%")
+            if percentage > 90:
+                print(f"Files {file} and {file2} are really similar")
+                answer = input("Do you want to delete one of them? [Y/n]: ")
+                if answer.lower() == 'y' or answer.lower() == 'yes' or answer == '':
+                    if len(f1) > len(f2):
+                        os.remove(file)
+                        print(f"{file} removed")
+                    else:
+                        os.remove(file2)
+                        print(f"{file2} removed")
 if __name__ == '__main__':
     loop = True
     while loop:
-        try:
-            similarFiles(os.getcwd())
-            loop = False
-        except FileNotFoundError:
-            print("Folder not found")
+        similarFiles(os.getcwd())
+        loop = False
